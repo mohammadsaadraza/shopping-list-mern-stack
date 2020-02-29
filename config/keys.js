043d934bd3379d148f438739ecee1c6c7c5e47cc
Saad Raza <saad.dr98@gmail.com>
2020-02-29 22:48:45 +0500

    Version 1.0.0

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-    mongoURI : 'mongodb+srv://saad:mongo12345@mernstack-rmtj8.mongodb.net/test?retryWrites=true&w=majority'
+    mongoURI : ''//enter your own mongoDb URI here
 }
